9a665ffed428b65deaff7100a71fe79c1cbd7893
Christopher Nethercott <ccnethercott@gmail.com>
2023-11-19 21:30:01 +0000

    fix: update ./run.sh to use the local extra_vars.yml file

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-ansible-playbook --ask-vault-pass --extra-vars @extra_vars.yml -i inventory.ini main.yml
+ansible-playbook --ask-vault-pass --extra-vars @./extra_vars.yml -i inventory.ini main.yml
